7abd67a545ddf966ce1f84a32a5d93220c395e63
Siva-Komma <kommasivajyothi22@gmail.com>
2023-02-25 19:34:18 +0530

    Adding data Into stack

--- StackandQueue/Program.cs
+++ StackandQueue/Program.cs
@@ -11,6 +11,11 @@ namespace StackandQueue
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack and Queue using Linked List");
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
             Console.ReadLine();
         }
     }
